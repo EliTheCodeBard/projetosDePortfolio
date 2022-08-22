89cf0ac74cf4996f6580f0245cd618dfbeacf3ef
Eliane de Oliveira <98899850+EliTheCodeBard@users.noreply.github.com>
2022-08-22 18:44:50 -0300

    Add files via upload
    Upload do primeiro projeto de três na montagem de um portfólio.

---
+++ ProjetoPortfolioJS/urna/etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulana de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'prefeita-redim.png', legenda: 'Vereadora'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrana da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'vice-prefeita-redim.png', legenda: 'Vereadora'}
+                ]
+            },
+        ] 
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclana',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'prefeita-redim.png', legenda: 'Prefeita'},
+                    {url:'vice-prefeita-redim.png', legenda: 'Vice-prefeita', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulana',
+                partido: 'QWERL',
+                vice: 'Zul',
+                fotos: [
+                    {url:'vice-prefeita.png', legenda: 'Prefeita'},
+                    {url:'prefeita-redim.png', legenda: 'Vice-prefeita', small: true}
+                ]
+            }
+        ]
+    }
+    
+]
